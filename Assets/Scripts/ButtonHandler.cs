b956db4dc6be8994e5db91f984209fa36392e7d4
yu1mu <all0fus168@gmail.com>
2022-06-01 20:24:56 +0900

    add : return Button

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -6,18 +6,36 @@ public class ButtonHandler : MonoBehaviour
 {
     private DisplayImage currentDisplay;
 
+    private float initialCameraSize;
+    private Vector3 initialCameraposition;
+
     void Start()
     {
         currentDisplay = GameObject.Find("displayImage").GetComponent<DisplayImage>();
+
+        initialCameraSize = Camera.main.orthographicSize;
+        initialCameraposition = Camera.main.transform.position;
     }
 
-    public void OnRightClickArrow()
+    public void OnClickRightArrow()
     {
         currentDisplay.CurrentWall = currentDisplay.CurrentWall + 1;
     }
 
-    public void OnLeftClickArrow()
+    public void OnClickLeftArrow()
     {
         currentDisplay.CurrentWall = currentDisplay.CurrentWall - 1;
+    }
+    
+    public void OnClickZoomReturn()
+    {
+        GameObject.Find("displayImage").GetComponent<DisplayImage>().CurrentState = DisplayImage.State.normal;
+        var zoomInObjects = FindObjectsOfType<ZoomInObject>();
+        foreach(var zoomInObject in zoomInObjects)
+        {
+            zoomInObject.gameObject.layer = 0;
+        }
+        Camera.main.orthographicSize = initialCameraSize;
+        Camera.main.transform.position = initialCameraposition;
     }
 }
